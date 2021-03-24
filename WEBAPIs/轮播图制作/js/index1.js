458f71c98b90bb9e41af04ed162d112b30764632
tomret <tomret@126.com>
2021-03-24 18:03:48 +0800

    [add]【自己】【轮播图】模块一【小圆圈】

--- WEBAPIs/轮播图制作/js/index1.js
+++ WEBAPIs/轮播图制作/js/index1.js
@@ -1,14 +1,23 @@
 window.addEventListener('load', () => {
+    // 1.1.获取元素
     var focus = document.querySelector('.focus');
     var leftArrow = document.querySelector('.arrow-l');
-    var rightArrow = document.querySelector('.arrow-r')
-    console.log(focus)
+    var rightArrow = document.querySelector('.arrow-r');
+
+    // 1.2.鼠标经过 focus 显示隐藏左右按钮
     focus.addEventListener('mouseenter', () => {
         leftArrow.style.display = 'block'
         rightArrow.style.display = 'block'
     })
     focus.addEventListener('mouseleave', () => {
-        leftArrow.style.display = 'none'
-        rightArrow.style.display = 'none'
-    })
+            leftArrow.style.display = 'none'
+            rightArrow.style.display = 'none'
+        })
+        //1.3动态生成小圆圈
+    var lis = focus.querySelector('ul').querySelectorAll('li');
+    var ol = focus.querySelector('ol')
+    for (var i = 0; i < lis.length; i++) {
+        var li = document.createElement('li');
+        ol.appendChild(li);
+    }
 })
