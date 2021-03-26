ffbee831a509ec60843abfb773144a58c269b490
tomret <tomret@126.com>
2021-03-26 16:07:12 +0800

    [modify]【答案】【购物车】【模块二】【增加减少【 小计】输入数字功能】

--- Jquery/购物车/js/car1.js
+++ Jquery/购物车/js/car1.js
@@ -20,9 +20,10 @@ $(function() {
         //模块三小计模块
         var p = $(this).parent().parent().siblings('.p-price').html();
         var p = p.substr(1);
-        console.log(n);
+        // console.log(n);
         //小计模块
-        $(this).parent().parent().siblings('.p-sum').html('$' + p * n);
+        $(this).parents('.p-num').siblings('.p-sum').html('$' + p * n);
+        console.log($(this).parent().parent('.p-sum'));
     })
     $('.decrement').click(function() {
         var n = $(this).siblings('.itxt').val();
@@ -33,11 +34,21 @@ $(function() {
         n--;
         $(this).siblings('.itxt').val(n);
 
-        var p = $(this).parent().parent().siblings('.p-price').html();
+        var p = $(this).parents('.p-num').siblings('.p-price').html();
         var p = p.substr(1);
-        console.log(n);
+        // console.log(n);
         //小计模块
-        $(this).parent().parent().siblings('.p-sum').html('$' + p * n);
+        $(this).parents('.p-num').siblings('.p-sum').html('$' + p * n);
+    })
+    $('.itxt').change(function() {
+        //先得到文本框里面的值 乘商品单价
+        //下面都是cv上面 过来的 
+        var n = $(this).val();
+        var p = $(this).parents('.p-num').siblings('.p-price').html();
+        var p = p.substr(1); //去人民币符号
+        $(this).parents('.p-num').siblings('.p-sum').html('$' + p * n); //更改值
+
+
     })
 
 })
