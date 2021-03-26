1a3501f1bd7b774ae0c34acf5a34c752945a1ff0
tomret <tomret@126.com>
2021-03-26 18:17:47 +0800

    [modify]【答案】【购物车】【合计数额 价钱】

--- Jquery/购物车/js/car1.js
+++ Jquery/购物车/js/car1.js
@@ -1,4 +1,5 @@
 $(function() {
+    // var sum = 0;
     $('.checkall').change(function() {
         var status = $(this).prop('checked');
         $('.j-checkbox ,.checkall').prop('checked', status);
@@ -16,14 +17,18 @@ $(function() {
     $('.increment').click(function() {
         var n = $(this).siblings('.itxt').val();
         n++;
+        // sum = n;
         $(this).siblings('.itxt').val(n);
         //模块三小计模块
-        var p = $(this).parent().parent().siblings('.p-price').html();
+        var p = $(this).parents('.p-num').siblings('.p-price').html();
         var p = p.substr(1);
         // console.log(n);
         //小计模块
-        $(this).parents('.p-num').siblings('.p-sum').html('$' + p * n);
-        console.log($(this).parent().parent('.p-sum'));
+        var price = (p * n).toFixed(2)
+        $(this).parents('.p-num').siblings('.p-sum').html('$' + price);
+        // console.log($(this).parent().parent('.p-sum'));
+        // console.log('sum:', sum);
+        getSum();
     })
     $('.decrement').click(function() {
         var n = $(this).siblings('.itxt').val();
@@ -32,23 +37,51 @@ $(function() {
             return false;
         }
         n--;
+        // sum = n;
         $(this).siblings('.itxt').val(n);
 
         var p = $(this).parents('.p-num').siblings('.p-price').html();
         var p = p.substr(1);
         // console.log(n);
         //小计模块
-        $(this).parents('.p-num').siblings('.p-sum').html('$' + p * n);
+        var price = (p * n).toFixed(2)
+        $(this).parents('.p-num').siblings('.p-sum').html('$' + price);
+        console.log('sum:', sum);
+        getSum();
     })
     $('.itxt').change(function() {
         //先得到文本框里面的值 乘商品单价
         //下面都是cv上面 过来的 
         var n = $(this).val();
+        // sum = n;
         var p = $(this).parents('.p-num').siblings('.p-price').html();
         var p = p.substr(1); //去人民币符号
-        $(this).parents('.p-num').siblings('.p-sum').html('$' + p * n); //更改值
-
+        var price = (p * n).toFixed(2)
+        $(this).parents('.p-num').siblings('.p-sum').html('$' + p); //更改值
+        //5.计算总计和总额模块
+        // console.log('sum:', sum);
+        getSum();
 
     })
+
+    getSum(); //第一次调用 
+    //5.计算总计和总额模块
+    function getSum() {
+        //？为什么不能全局变量 sum
+        var count = 0; //计算总件数
+        var money = 0; //计算总金额
+        $('.itxt').each(function(i, ele) {
+            count += parseInt($(ele).val());
+            $('.amount-sum em').text(count);
+        })
+        console.log('sum', $('.p-sum'));
+        $('.p-sum').each(function(i, ele) {
+            console.log('this', $(ele));
+
+            money += parseFloat($(ele).text().substr(1));
+            $('.price-sum em').text('$' + money);
+            console.log(money);
+        })
+    }
 
 })
