c290b6347b6086670e476a8c337712b9540e46b4
tomret <tomret@126.com>
2021-03-27 17:11:09 +0800

    [modify]【答案】【品优购】【电梯导航】【按钮】

--- Jquery/案例电梯导航/09-电梯导航/js/index1.js
+++ Jquery/案例电梯导航/09-电梯导航/js/index1.js
@@ -10,14 +10,15 @@ $(function() {
         } else {
             $('.fixedtool').fadeOut();
         }
-        $('.fixedtool ul').on('click', 'li', function() {
+        // 获取每个li
+        $('.fixedtool li').on('click', function() {
             //获取对应li的索引号
             var index = $(this).index();
-            var dis = $('.floor').children().eq(index).scrollTop();
-            var offsetDis = $('.floor').children().eq(index).offset().top;
-            console.log(offsetDis);
-            $('body, html').animate({
-                top: offsetDis
+            var offsetDis = $('.floor .w').eq(index).offset().top;
+
+            // console.log(offsetDis);
+            $('body, html').stop().animate({
+                scrollTop: offsetDis //就是滚动多少距离
             })
         })
     })
