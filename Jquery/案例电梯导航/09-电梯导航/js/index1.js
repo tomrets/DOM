25c9a68fe8b34d566fcd3744844b346268f896ac
tomret <tomret@126.com>
2021-03-27 17:33:05 +0800

    [modify]【答案】【品优购】【电梯导航】【按钮变色 修复问题】

--- Jquery/案例电梯导航/09-电梯导航/js/index1.js
+++ Jquery/案例电梯导航/09-电梯导航/js/index1.js
@@ -1,21 +1,26 @@
 $(function() {
-    $(window).on('scroll', function() {
-        var toolTop = $('.recommend').offset().top;
-        // console.log(toolTop1);
-        // var toolTop = $('.recommend').scrollTop();
-        // console.log(toolTop);
+    var toolTop = $('.recommend').offset().top;
+    toggleTool();
+
+    function toggleTool() {
 
         if ($(document).scrollTop() >= toolTop) {
             $('.fixedtool').fadeIn();
         } else {
             $('.fixedtool').fadeOut();
         }
+    }
+    $(window).on('scroll', function() {
+        toggleTool();
+
         // 获取每个li
         $('.fixedtool li').on('click', function() {
             //获取对应li的索引号
             var index = $(this).index();
             var offsetDis = $('.floor .w').eq(index).offset().top;
 
+            //链式编程
+            $(this).addClass('current').siblings().removeClass('current');
             // console.log(offsetDis);
             $('body, html').stop().animate({
                 scrollTop: offsetDis //就是滚动多少距离
