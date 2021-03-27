17dd3c41371e29453314a64d746165f1a66eba33
tomret <tomret@126.com>
2021-03-27 18:00:36 +0800

    [add]【自己】【品优购】【电梯导航】【内容控制按钮】

--- Jquery/案例电梯导航/09-电梯导航/js/index1.js
+++ Jquery/案例电梯导航/09-电梯导航/js/index1.js
@@ -13,6 +13,20 @@ $(function() {
     $(window).on('scroll', function() {
         toggleTool();
 
+        //内容让模块变色
+        $('.floor .w').each(function(i, ele) {
+            var offsetDis = $(ele).offset().top;
+            var scrollDis = $('body,html').scrollTop();
+            // console.log(offsetDis);
+            // console.log(slideDis);
+
+            if (scrollDis >= offsetDis) {
+                console.log(i);
+                $('.fixedtool li').eq(i).addClass('.current').siblings().removeClass('.current');
+
+            }
+        })
+
         // 获取每个li
         $('.fixedtool li').on('click', function() {
             //获取对应li的索引号
